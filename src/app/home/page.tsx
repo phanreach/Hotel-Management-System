cb0728490a0947dfac3373ec34b4c10fdb266f04
phanreach <reach4phan@gmail.com>
2025-12-20 11:58:38 +0700

    initial commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+import Card from "@/src/components/card";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>hello from home page</h1>
+      <Card />
+    </div>
+  );
+}
---
+++ src/components/card.tsx
@@ -0,0 +1,7 @@
+export default function Card() {
+  return (
+    <div>
+      <h1>Card component</h1>
+    </div>
+  );
+}
