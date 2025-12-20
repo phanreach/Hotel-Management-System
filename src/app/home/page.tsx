4227a87644e95fd0177197652c99cfc74e5fe4e0
phanreach <reach4phan@gmail.com>
2025-12-20 13:17:33 +0700

    add tailwind

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,7 +3,7 @@ import Card from "@/src/components/card";
 export default function Home() {
   return (
     <div>
-      <h1>hello from home page</h1>
+      <h1 className="font-semibold">hello from home page</h1>
       <Card />
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="font-semibold">hello</div>;
+  return <div className="font-bold">hello</div>;
 }
